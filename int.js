518d8c42c60d25a48817da2a674aaac1b1ba6f03
marwenghariani <100080058+marwenghariani@users.noreply.github.com>
2022-09-14 10:13:24 +0100

    Add files via upload

---
+++ int.js
@@ -0,0 +1,16 @@
+var x = 7, y = 14;
+
+var vrai = x < y;
+var faux = 14 <= 7;
+
+var egalval = 4 == "4"; 
+var egalvaltype = 4 !== "4";
+
+var difval = 4 != "4";
+var difvaltype = 4 !== "4";
+alert("vrai stocke : " + vrai +
+'\nfaux stocke : ' + faux + 
+"\negalval stocke : " + egalval +
+"\negalvaltype stocke : " + egalvaltype +
+"\ndifval stocke : " + difval +
+"\ndifvaltype : " + difvaltype ) ;
---
+++ int2.js
@@ -0,0 +1,11 @@
+var heure = 9
+if( heure >= 0 && heure <= 12)
+{ alert("c\' le matin ");}
+else if( heure === 12 )
+        {alert(" c'est midi "); }
+ else if( heure >= 12 && heure <= 18 )
+        { alert("c'est l'aprés midi ");}
+else if(heure >= 18 && heure <= 24)
+        { alerte ("c le soir ");}
+
+    else {(alerte(" heure non valid "));}
